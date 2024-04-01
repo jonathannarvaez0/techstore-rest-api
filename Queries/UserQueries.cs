d83dd12388b3c627f4fe4f033d5d4382e2bdf77c
Jonathan Narvaez <narvaez.jonathan18@gmail.com>
2024-04-01 13:49:18 +0800

    Add: endpoint for relogin giving out acc details via sessionId

--- Queries/UserQueries.cs
+++ Queries/UserQueries.cs
@@ -4,5 +4,6 @@
     {
         public static string LoginAccount = "SELECT record_id,username,email,first_name,last_name,contact_no FROM dbo.userinfo WHERE username = @username AND password = @password";
         public static string CreateSessionId = "INSERT INTO dbo.sessions (user_id, session_id) VALUES (@userId, @sessionId)";
+        public static string LoginUsingSession = "SELECT user_id, username, email,first_name,last_name, contact_no, session_id from dbo.sessions inner join dbo.userinfo on dbo.userinfo.record_id = dbo.sessions.user_id WHERE session_id = @sessionId";
     }
 }
